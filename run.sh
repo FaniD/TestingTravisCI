2978791e53785acaedd293f34fe07b4afbbf78f3
FaniD <fani.dimou92@gmail.com>
2019-05-21 00:18:04 +0300

    new test

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #And now run the container
 docker build . -t demo
-docker run -d -p 8887:8887 --pid="host" --privileged demo
+docker run -d -p 8887:8887 --pid="host" --privileged --name demo demo
 ./curl_it.sh
 docker kill demo
